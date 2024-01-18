6940d29b802f89303151b8ff727bc2077b9071b8
Den-Sec <156823722+Den-Sec@users.noreply.github.com>
2024-01-18 13:21:48 +0100

    BLOOMBOX.ino
    Signed-off-by: Den-Sec <156823722+Den-Sec@users.noreply.github.com>

--- src/BLOOMBOX.ino
+++ src/BLOOMBOX.ino
@@ -2,110 +2,119 @@
 #include <DHT.h>
 #include "RTClib.h"
 #include <Adafruit_MLX90614.h>
-
+#include <SPI.h>
+#include <SD.h>
 
 #define DHTPIN 0         // DHT11 connected to GPIO0
 #define DHTTYPE DHT11    // DHT 11
-//#define RELAY_PIN1 4     // Relay connected 
-//#define RELAY_PIN2 16    // Relay connected
+#define RELAY_PIN1 4     // Relay connected 
+#define RELAY_PIN2 16    // Relay connected
 #define RELAY_PIN3 17    // Relay connected
 #define RELAY_PIN4 5     // Relay connected
-#define TEMP_LOW_THRESHOLD 14.0 // Temperature threshold
-#define TEMP_HIGH_THRESHOLD 14.5 // Temperature threshold
 #define WATER_LEVEL_PIN 35  // Water level sensor connected to GPIO35
 #define WATER_SENSOR_POWER_PIN 32  // Power pin for the water level sensor connected to GPIO32
 #define SOIL_MOISTURE_PIN 34 // Soil moisture sensor connected to GPIO34
-
-
-#define WATER_LEVEL_MAX 305  // Maximum water level reading
-#define WATER_LEVEL_MIN 120     // Minimum water level reading
-
-
-// Initialize water level percentage variable
-int waterLevelPercent;
-
-
-int waterLevelFinal;
-unsigned long waterLevelCheckTime = 60000; // 1 minute, for example
-unsigned long lastCheck = 0;
-
+#define CS_PIN 13 // SD card pin definitions
 
 DHT dht(DHTPIN, DHTTYPE);
 RTC_DS3231 rtc;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
+// Global Variables
+File logFile;
+int level25, level50, level75;
+unsigned long waterLevelCheckTime;
+int WATER_LEVEL_MAX, WATER_LEVEL_MIN;
+float TEMP_LOW_THRESHOLD, TEMP_HIGH_THRESHOLD;
 
 void setup() {
   Serial.begin(115200);
-  
+
+  // Initialize the DHT
   dht.begin();
+ 
+  // Initialize the MLX
+  mlx.begin();
 
+  // Initialize the SD
+  SD.begin(CS_PIN);
 
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
+  // Create "Logs" directory if it doesn't exist
+  if (!SD.exists("/Logs")) {
+    SD.mkdir("/Logs");
   }
 
-
-  if (!mlx.begin()) {
-    Serial.println("Failed to start MLX90614");
-    while (1);
+  // Create "Logs" directory if it doesn't exist
+  if (!SD.exists("/Config")) {
+    SD.mkdir("/Config");
   }
 
+  // Initialize the RTC
+  rtc.begin();
 
-  //pinMode(RELAY_PIN1, OUTPUT); // Set the relay pin as output
-  //pinMode(RELAY_PIN2, OUTPUT); // Set the relay pin as output
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, let's set the time!");
+    // Set the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  // Get the current date and time
+  DateTime now = rtc.now();
+
+  // Format the date and time
+  char dateTimeString[20]; // Adjust the size based on your date and time format
+  sprintf(dateTimeString, "%04d/%02d/%02d %02d:%02d:%02d", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+
+  // Create the log message
+  String logMessage = String(dateTimeString) + " - Starting Up..";
+
+  // Write to SD card and Serial
+  writeToSDCard(logMessage.c_str());
+  Serial.println(logMessage);
+
+  pinMode(RELAY_PIN1, OUTPUT); // Set the relay pin as output
+  pinMode(RELAY_PIN2, OUTPUT); // Set the relay pin as output
   pinMode(RELAY_PIN3, OUTPUT); // Set the relay pin as output
   pinMode(RELAY_PIN4, OUTPUT); // Set the relay pin as output
   pinMode(WATER_LEVEL_PIN, INPUT); // Set the water level pin as input
   pinMode(SOIL_MOISTURE_PIN, INPUT); // Set the soil moisture sensor pin as input
   pinMode(WATER_SENSOR_POWER_PIN, OUTPUT);
-  digitalWrite(WATER_SENSOR_POWER_PIN, LOW);  // Initially turn off the sensor
-  //digitalWrite(WATER_SENSOR_POWER_PIN, HIGH);
-
-
+  digitalWrite(WATER_SENSOR_POWER_PIN, LOW);  // Initially turn off the water level sensor
+  //digitalWrite(WATER_SENSOR_POWER_PIN, HIGH); // Initially turn on the water level sensor
 
   // Turn off relays initially
-  //digitalWrite(RELAY_PIN1, HIGH);
-  //digitalWrite(RELAY_PIN2, HIGH);
+  digitalWrite(RELAY_PIN1, HIGH);
+  digitalWrite(RELAY_PIN2, HIGH);
   digitalWrite(RELAY_PIN3, HIGH);
   digitalWrite(RELAY_PIN4, HIGH);
 
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+  // Load configuration
+  loadConfiguration("/Config/config.txt");
 }
-
 
 void loop() {
   delay(1000); // Wait a few seconds between measurements
 
+  static unsigned long lastCheck = 0;
+  unsigned long waterLevelCheckTime = 10000; // Delay for Logs, 60000=1 minute
 
+  DateTime now = rtc.now(); // Get current time
   float humidity = dht.readHumidity(); // Reading humidity
   float ambientTemp = mlx.readAmbientTempC(); // Reading ambient temperature
-  int waterLevel = analogRead(WATER_LEVEL_PIN); // Reading water level
   int soilMoisture = analogRead(SOIL_MOISTURE_PIN); // Reading soil moisture
+  int waterLevelRaw = analogRead(WATER_LEVEL_PIN);   // Reading water level
+  int waterLevel;
 
-
-  // Reading water level
-  int waterLevelRaw = analogRead(WATER_LEVEL_PIN); 
-
-
-  // Calibration data
+  // Water Sensor Calibration data
   const int level25 = 205; 
   const int level50 = 255; 
   const int level75 = 280; 
 
-
-// Check if it's time to calculate the final water level
+  // Check if it's time to calculate the final water level
   if (millis() - lastCheck >= waterLevelCheckTime) {
-
 
     digitalWrite(WATER_SENSOR_POWER_PIN, HIGH);  // Turn on the sensor
     delay(30);  // Short delay for power stabilization
-
 
     int sum = 0;
     for (int i = 0; i < 5; i++) {
@@ -113,87 +122,48 @@ void loop() {
       delay(3000); // Wait for 3 seconds
     }
 
-
     digitalWrite(WATER_SENSOR_POWER_PIN, LOW);  // Turn off the sensor
 
+    waterLevel = sum / 5; // Calculate average
 
-    waterLevelFinal = sum / 5; // Calculate average
-
-
-    if (waterLevelFinal < level25) {
-      waterLevelFinal = map(waterLevelFinal, WATER_LEVEL_MIN, level25, 0, 25);
+    if (waterLevel < level25) {
+      waterLevel = map(waterLevel, WATER_LEVEL_MIN, level25, 0, 25);
     } else if (waterLevelRaw < level50) {
-      waterLevelFinal = map(waterLevelFinal, level25, level50, 25, 50);
+      waterLevel = map(waterLevel, level25, level50, 25, 50);
     } else if (waterLevelRaw < level75) {
-      waterLevelFinal = map(waterLevelFinal, level50, level75, 50, 75);
+      waterLevel = map(waterLevel, level50, level75, 50, 75);
     } else {
-      waterLevelFinal = map(waterLevelFinal, level75, WATER_LEVEL_MAX, 75, 100);
+      waterLevel = map(waterLevel, level75, WATER_LEVEL_MAX, 75, 100);
     }
 
+    waterLevel = constrain(waterLevel, 0, 100); // Constrain to 0-100%
 
-    waterLevelFinal = constrain(waterLevelFinal, 0, 100); // Constrain to 0-100%
+    if (isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+    }
 
+    // Create data string
+    String dataString = "";
+    dataString += String(now.year(), DEC) + "/";
+    dataString += String(now.month(), DEC) + "/";
+    dataString += String(now.day(), DEC) + " ";
+    dataString += String(now.hour(), DEC) + ":";
+    dataString += String(now.minute(), DEC) + ":";
+    dataString += String(now.second(), DEC) + " - Humidity: ";
+    dataString += String(humidity) + " %, Ambient Temp: ";
+    dataString += String(ambientTemp) + " C, Water Level Percent: ";
+    dataString += String(waterLevel) + " %";
+    dataString += ", Soil Moisture: " + String(soilMoisture);
 
-
-    // Print the final water level
-    Serial.print("Final Water Level: ");
-    Serial.println(waterLevelFinal);
+    // Print to Serial
+    Serial.println(dataString);
+    
+    writeToSDCard(dataString.c_str());
 
 
     lastCheck = millis(); // Update the last check time
   }
-
-
-// Convert raw readings to percentage based on calibration data
-  if (waterLevelRaw < level25) {
-    waterLevelPercent = map(waterLevelRaw, WATER_LEVEL_MIN, level25, 0, 25);
-  } else if (waterLevelRaw < level50) {
-    waterLevelPercent = map(waterLevelRaw, level25, level50, 25, 50);
-  } else if (waterLevelRaw < level75) {
-    waterLevelPercent = map(waterLevelRaw, level50, level75, 50, 75);
-  } else {
-    waterLevelPercent = map(waterLevelRaw, level75, WATER_LEVEL_MAX, 75, 100);
-  }
-
-
-  waterLevelPercent = constrain(waterLevelPercent, 0, 100); // Constrain to 0-100%
-
-
-
-  if (isnan(humidity)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-
-  DateTime now = rtc.now(); // Get current time
-
-
-  // Print the humidity, ambient temperature and time
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.print(" - Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" %, Ambient Temp: ");
-  Serial.print(ambientTemp);
-  Serial.print(" C");
-  Serial.print(", Water Level: ");
-  Serial.print(waterLevel);
-  Serial.print(", Water Level Percent: ");
-  Serial.print(waterLevelPercent);
-  Serial.print(" %");
-  Serial.print(", Soil Moisture: ");
-  Serial.println(soilMoisture);
-
 
   // Control the relays based on temperature
   if (ambientTemp > TEMP_HIGH_THRESHOLD) {
@@ -205,3 +175,77 @@ void loop() {
   }
 }
 
+void writeToSDCard(const char * dataString) {
+    Serial.println("Appending to file: /Logs/log.txt");
+
+    File logFile = SD.open("/Logs/log.txt", FILE_APPEND);
+    if(!logFile){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(logFile.println(dataString)){
+        Serial.println("Data appended to file");
+    } else {
+        Serial.println("Append failed");
+    }
+    logFile.close();
+}
+
+void loadConfiguration(const char* filename) {
+  Serial.print("Loading configuration from: ");
+  Serial.println(filename);
+
+  File configFile = SD.open(filename);
+  if (!configFile) {
+    Serial.println("Failed to open configuration file");
+    return;
+  }
+
+  while (configFile.available()) {
+    String line = configFile.readStringUntil('\n');
+    line.trim(); // Trim whitespace from the line
+
+    if (line.length() == 0 || line.startsWith("#")) {
+      // Skip empty lines and comments
+      continue;
+    }
+
+    int index = line.indexOf('='); // Search for the position of the first '=' character in the line
+    // Check if the '=' character is found in the line
+    if (index == -1) {
+      // If '=' is not found, print a message to the Serial Monitor
+      Serial.println("No '=' found in line, skipping");
+      
+      // Skip the rest of the loop iteration because the line does not contain
+      // a valid key-value pair (i.e., it's not in the expected "key=value" format)
+      continue;
+    }
+
+    String key = line.substring(0, index);
+    String value = line.substring(index + 1);
+
+    Serial.print("Key: ");
+    Serial.print(key);
+    Serial.print(", Value: ");
+    Serial.println(value);
+
+    if (key == "level25") {
+      level25 = value.toInt();
+    } else if (key == "level50") {
+      level50 = value.toInt();
+    } else if (key == "level75") {
+      level75 = value.toInt();
+    } else if (key == "waterLevelCheckTime") {
+      waterLevelCheckTime = value.toInt();
+    } else if (key == "WATER_LEVEL_MAX") {
+      WATER_LEVEL_MAX = value.toInt();
+    } else if (key == "WATER_LEVEL_MIN") {
+      WATER_LEVEL_MIN = value.toInt();
+    } else if (key == "TEMP_LOW_THRESHOLD") {
+      TEMP_LOW_THRESHOLD = value.toFloat();
+    } else if (key == "TEMP_HIGH_THRESHOLD") {
+      TEMP_HIGH_THRESHOLD = value.toFloat();
+    }
+  }
+  configFile.close();
+}
